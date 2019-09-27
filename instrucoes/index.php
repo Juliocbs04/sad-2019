bf987ff0206c8a43d95fa6c5089ea51818c0bae4
julio <profile.julio@gmail.com>
2019-09-27 20:28:14 -0300

    Segunda versão

---
+++ instrucoes/index.php
@@ -0,0 +1,16 @@
+<?php
+    echo calcular(8,16.98);
+
+    function calcular($quantidade,$precoUnit){
+        return $quantidade * $precoUnit;
+    }
+    function fatorial($n){
+        if($n==0){
+            return 'O fatorial é 1';
+        }
+        for($i=0;$i<$n;$i++){   
+            
+        }
+    }
+
+?>
